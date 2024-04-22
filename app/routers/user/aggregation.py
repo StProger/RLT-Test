d74dc51c86a4a899f39079a4ee473fe62c15989d
Kirill Mikhailov <kicode@mail.ru>
2024-04-22 23:11:41 +0300

    add valid data

--- app/routers/user/aggregation.py
+++ app/routers/user/aggregation.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F, types
 
 from app.utils.aggregation_salary import aggregation_salary
+from app.settings import settings
 
 import ast
 
@@ -11,6 +12,15 @@ router = Router()
 @router.message()
 async def aggregation_handler(message: types.Message):
 
-    data = ast.literal_eval(message.text.replace("\n", ""))
-    output_data = await aggregation_salary(data=data)
-    await message.answer(str(output_data))
+    text = """Невалидный запрос. Пример запроса:
+{"dt_from": "2022-09-01T00:00:00", "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}"""
+    try:
+        data = ast.literal_eval(message.text.replace("\n", ""))
+    except ValueError:
+        await message.answer(text)
+        return
+    if data not in settings.valid_data:
+        await message.answer(text)
+    else:
+        output_data = await aggregation_salary(data=data)
+        await message.answer(str(output_data))
--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,26 @@ class Settings(BaseSettings):
     # Путь к логам
     PATH_LOGS: str = "bot/data/logs.log"
 
+    # Валидные данные
+    valid_data: list[dict] = [
+        {
+            "dt_from": "2022-02-01T00:00:00",
+            "dt_upto": "2022-02-02T00:00:00",
+            "group_type": "hour"
+        },
+        {
+            "dt_from": "2022-09-01T00:00:00",
+            "dt_upto": "2022-12-31T23:59:00",
+            "group_type": "month"
+        },
+        {
+            "dt_from": "2022-10-01T00:00:00",
+            "dt_upto": "2022-11-30T23:59:00",
+            "group_type": "day"
+        }
+    ]
+
+
     class Config:
 
         env_file = ".env"
