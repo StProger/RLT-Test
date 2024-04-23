d06e07f950e71a99e1776a815d63c0cce0f05ae2
Kirill Mikhailov <kicode@mail.ru>
2024-04-23 11:59:16 +0300

    fix

--- app/routers/user/aggregation.py
+++ app/routers/user/aggregation.py
@@ -25,14 +25,14 @@ async def aggregation_handler(message: types.Message):
         await message.answer(text)
         return
 
-    if data not in settings.valid_data:
-        print(f"Неправильный запрос: {message.text}")
-        text = """Допустимо отправлять только следующие запросы:
-{"dt_from": "2022-09-01T00:00:00", "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}
-{"dt_from": "2022-10-01T00:00:00", "dt_upto": "2022-11-30T23:59:00", "group_type": "day"}
-{"dt_from": "2022-02-01T00:00:00", "dt_upto": "2022-02-02T00:00:00", "group_type": "hour"}"""
-
-        await message.answer(text)
+#     if data not in settings.valid_data:
+#         print(f"Неправильный запрос: {message.text}")
+#         text = """Допустимо отправлять только следующие запросы:
+# {"dt_from": "2022-09-01T00:00:00", "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}
+# {"dt_from": "2022-10-01T00:00:00", "dt_upto": "2022-11-30T23:59:00", "group_type": "day"}
+# {"dt_from": "2022-02-01T00:00:00", "dt_upto": "2022-02-02T00:00:00", "group_type": "hour"}"""
+#
+#         await message.answer(text)
     else:
         output_data = await aggregation_salary(data=data)
         print(str(output_data).replace("'", "\""))
